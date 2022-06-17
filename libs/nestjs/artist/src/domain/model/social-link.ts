ba5b0f4124f975ba6dc26c9878e2f813c2ee22cc
josefrnandezz <josefernandez@genially.com>
2022-06-17 19:29:29 +0200

    fix: fix regex and condition

--- libs/nestjs/artist/src/domain/model/social-link.ts
+++ libs/nestjs/artist/src/domain/model/social-link.ts
@@ -4,9 +4,9 @@ interface Props {
   value: string;
 }
 
-const spotifyRegex = /https:\/\/(www\.)?open\.spotify\.com\/artist/;
-const youtubeRegex = /https:\/\/(www\.)?youtube\.com\/c/;
-const souncloudRegex = /https:\/\/(www\.)?soundcloud\.com/;
+const spotifyRegex = /https:\/\/(www\.)?open\.spotify\.com\/artist\/.*/;
+const youtubeRegex = /https:\/\/(www\.)?youtube\.com\/c\/.*/;
+const souncloudRegex = /https:\/\/(www\.)?soundcloud\.com\/.*/;
 
 export class SocialLink extends ValueObject<Props> {
   public static fromString(link: string): SocialLink {
@@ -21,8 +21,8 @@ export class SocialLink extends ValueObject<Props> {
 
   private static validateSupportedSocialLinks(link: string) {
     if (
-      !spotifyRegex.test(link) ||
-      !youtubeRegex.test(link) ||
+      !spotifyRegex.test(link) &&
+      !youtubeRegex.test(link) &&
       !souncloudRegex.test(link)
     ) {
       throw DomainError.because('Invalid social link');
